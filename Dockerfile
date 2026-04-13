2f75916cddb701e430399305510361e46c539ee5
akhmadiev <akhmadiev@tax-monitor.ru>
2026-04-13 09:50:33 +0400

    init commit 2

--- Dockerfile
+++ Dockerfile
@@ -38,4 +38,4 @@ USER appuser
 EXPOSE 8000
 
 # Запуск приложения (используем модуль app)
-CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/auth.py
+++ app/auth.py
@@ -1,10 +1,10 @@
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Depends
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from app.config import settings
 
 security = HTTPBasic()
 
-def verify_credentials(credentials: HTTPBasicCredentials):
+def verify_credentials(credentials: HTTPBasicCredentials = Depends(security)):
     correct_username = settings.AUTH_USERNAME
     correct_password = settings.AUTH_PASSWORD
     if credentials.username != correct_username or credentials.password != correct_password:
